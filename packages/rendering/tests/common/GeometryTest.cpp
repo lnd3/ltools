b6fb8ce9e36488df73c402c32429413962b892aa
lnd3 <linus.cumselius@gmail.com>
2024-08-18 23:11:52 +0200

    Add an empty ui container test.

--- packages/rendering/tests/common/GeometryTest.cpp
+++ packages/rendering/tests/common/GeometryTest.cpp
@@ -1,13 +1,10 @@
 #include "testing/Test.h"
-#include "logging/Log.h"
+#include "logging/LoggingAll.h"
 #include "rendering/Geometry.h"
-#include "logging/String.h"
 
 TEST(GeometryTest, GeometryParsing) {
     l::rendering::Mesh mesh;
     TEST_TRUE(mesh.Load("./tests/data/modelformats/old_truck/old_truck_2.dae"), "");
-
-    //mesh.AssimpImport("./tests/data/modelformats/old_truck/old_truck_2.dae");
 
     return 0;
 }
---
+++ packages/rendering/tests/common/UIContainerTest.cpp
@@ -0,0 +1,13 @@
+#include "testing/Test.h"
+#include "logging/LoggingAll.h"
+#include "rendering/ui/UIContainer.h"
+
+TEST(UIContainerTest, BasicSetup) {
+    l::ui::ContainerArea windowContainer;
+
+
+
+    return 0;
+}
+
+
