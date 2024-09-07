3dd35b4b73b215f1f5d1eafe0e9d1888c93e752c
lnd3 <linus.cumselius@gmail.com>
2024-09-07 02:52:47 +0200

    Add a midi linux dummy.

---
+++ packages/hid/source/linux/MidiLinux.cpp
@@ -0,0 +1,21 @@
+#include "logging/Log.h"
+
+#include "MidiLinux.h"
+
+namespace l::hid::midi {
+
+	std::unique_ptr<MidiManager> CreateMidiManager() {
+		return nullptr;
+	}
+
+	void MidiManagerLinux::RegisterCallback(CallbackFunction) {
+	}
+
+	uint32_t MidiManagerLinux::GetNumDevices() {
+		return 0;
+	}
+
+	void MidiManagerLinux::SendToDevice(uint32_t, uint32_t, uint32_t, uint32_t, uint32_t) {
+	}
+}
+
