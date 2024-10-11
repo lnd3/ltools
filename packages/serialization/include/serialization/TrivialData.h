98259ddcce237fde6a591a1f241af3e0c9011de0
lnd3 <linus.cumselius@gmail.com>
2024-10-11 04:25:07 +0200

    Add string_id and encode_html.

--- packages/serialization/include/serialization/TrivialData.h
+++ packages/serialization/include/serialization/TrivialData.h
@@ -3,25 +3,11 @@
 #include <string>
 #include <vector>
 #include <sstream>
+#include <unordered_map>
 
 #include "logging/String.h"
 
 namespace l::serialization {
-
-	template<size_t size>
-	std::pair<size_t, std::vector<std::string>> ParseTrivialData(std::stringstream& data, std::string_view separators) {
-
-		std::vector<std::string> out;
-
-		size_t counter = 0;
-		for (std::string line; std::getline(data, line, '\n');) {
-			auto elements = l::string::split(line, separators);
-			for (auto it : elements) {
-				out.push_back(std::string(it));
-			}
-		}
-
-		return {size, out};
-	}
-
+	std::vector<std::string> ParseTrivialData(std::stringstream& data, std::string_view separators);
+	std::unordered_map<uint32_t, std::string> ParseTrivialDataMap(std::stringstream& data, std::string_view separators);
 }
