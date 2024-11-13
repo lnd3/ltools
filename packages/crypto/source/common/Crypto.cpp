b5068737bb9e2353f3a2a60d2b7181814032ebe8
lnd3 <linus.cumselius@gmail.com>
2024-11-13 01:11:46 +0100

    Fix crypto test.

--- packages/crypto/source/common/Crypto.cpp
+++ packages/crypto/source/common/Crypto.cpp
@@ -2,7 +2,7 @@
 
 namespace l::crypto {
 
-	bool Crypto::Init() {
+	bool CryptoED25519::Init() {
 		memset(mSeed, 0, 32);
 		if (ed25519_create_seed(&mSeed[0])) {
 			return false;
@@ -17,5 +17,30 @@ namespace l::crypto {
 		return result;
 	}
 
+	void CryptoED25519::CreateKeys() {
+		ed25519_create_keypair(mPubKey, mPriKey, mSeed);
+	}
+
+	std::string_view CryptoED25519::Sign(std::string_view message) {
+		auto messagePtr = reinterpret_cast<const unsigned char*>(message.data());
+		ed25519_sign(mSign, messagePtr, message.size(), mPubKey, mPriKey);
+		return std::string_view(reinterpret_cast<const char*>(mSign), 64);
+	}
+
+	std::string_view CryptoED25519::GetPublicKey() {
+		return std::string_view(reinterpret_cast<const char*>(mPubKey), 32);
+	}
+
+	bool CryptoED25519::Verify(std::string_view signature, std::string_view message, std::string_view publicKey) {
+		unsigned char* pubKey = reinterpret_cast<unsigned char*>(const_cast<char*>(publicKey.data()));
+		if (publicKey.empty()) {
+			pubKey = mPubKey;
+		}
+
+		auto signPtr = reinterpret_cast<const unsigned char*>(signature.data());
+		auto messagePtr = reinterpret_cast<const unsigned char*>(message.data());
+		auto result = ed25519_verify(signPtr, messagePtr, message.size(), pubKey);
+		return result != 0;
+	}
 
 }
--- packages/crypto/tests/common/ed25519Test.cpp
+++ packages/crypto/tests/common/ed25519Test.cpp
@@ -8,8 +8,14 @@ using namespace l;
 
 TEST(Crypto, ed2519) {
 
-	crypto::Crypto crypto;
-	LOG(LogTest) << crypto.Sign("test");
+	std::string message = "test message";
+	crypto::CryptoED25519 crypto;
+	crypto.CreateKeys();
+	auto signature = crypto.Sign(message);
+	//LOG(LogTest) << signature;
+
+	TEST_FALSE(crypto.Verify(signature, message + "sd"), "");
+	TEST_TRUE(crypto.Verify(signature, message), "");
 
 	return 0;
 }
