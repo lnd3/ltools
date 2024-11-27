ee76c16854f47ce39af36c3dff48f16715bfb2b7
lnd3 <linus.cumselius@gmail.com>
2024-11-27 12:06:41 +0100

    Add base ascii encoder/decoder skeleton.

---
+++ packages/serialization/source/common/BaseAscii.cpp
@@ -0,0 +1,82 @@
+#include "serialization/BaseAscii.h"
+
+#include "logging/LoggingAll.h"
+
+#include <string>
+#include <vector>
+#include <sstream>
+
+namespace {
+	const char s_vecUpper[] = "0123456789ABCDEF";
+	const char s_vecLower[] = "0123456789abcdef";
+	const char s_array[256] = {
+		-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+		-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+		-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+		 0,  1,  2,  3,  4,  5,  6,  7,  8,  9, -1, -1, -1, -1, -1, -1,
+		-1, 10, 11, 12, 13, 14, 15, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+		-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+		-1, 10, 11, 12, 13, 14, 15, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+		-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+		-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+		-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+		-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+		-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+		-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+		-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+		-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+		-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1
+	};
+}
+namespace l::serialization {
+
+	std::string baseascii_encode(unsigned char* src, size_t len, bool tolowercase) {
+		std::string out;
+		const char* svec = tolowercase ? s_vecLower : s_vecUpper;
+		for (size_t i = 0; i < len; i++) {
+			auto c = src[i];
+			out += svec[(c >> 4) & 0xf];
+			out += svec[(c & 0xf)];
+		}
+		return out;
+	}
+
+	void baseascii_decode(unsigned char* dst, std::string_view src) {
+		auto size = src.size();
+		size = size - size % 2;
+		for (size_t i = 0; i < size; i+=2) {
+			auto high = s_array[static_cast<unsigned char>(src.at(i + 0))];
+			auto low = s_array[static_cast<unsigned char>(src.at(i + 1))];
+			if (high >= 0 && low >= 0) {
+				dst[i] = static_cast<unsigned char>((high << 4) | low);
+			}
+		}
+	}
+
+	std::string baseascii_encode(std::string_view src, bool tolowercase) {
+		std::string out;
+		const char* svec = tolowercase ? s_vecLower : s_vecUpper;
+		for (size_t i = 0; i < src.size(); i++) {
+			auto c = src[i];
+			out += svec[(c >> 4) & 0xf];
+			out += svec[(c & 0xf)];
+		}
+		return out;
+	}
+
+	std::string baseascii_decode(std::string_view src) {
+		std::string out;
+		auto size = src.size();
+		size = size - size % 2;
+		for (size_t i = 0; i < size; i += 2) {
+			auto high = s_array[static_cast<unsigned char>(src.at(i + 0))];
+			auto low = s_array[static_cast<unsigned char>(src.at(i + 1))];
+			if (high >= 0 && low >= 0) {
+				out += static_cast<unsigned char>((high << 4) | low);
+			}
+		}
+		return out;
+	}
+
+
+}
