abfade56239e0369242b0437973992d671153260
lnd3 <linus.cumselius@gmail.com>
2025-05-09 20:56:48 +0200

    Upgrade detector.

--- packages/nodegraph/include/nodegraph/operations/NodeGraphOpLogic.h
+++ packages/nodegraph/include/nodegraph/operations/NodeGraphOpLogic.h
@@ -41,8 +41,8 @@ namespace l::nodegraph {
             auto input2 = inputs.at(1).GetIterator(numSamples);
             auto output = outputs.at(0).GetIterator(numSamples);
             for (int32_t i = 0; i < numSamples; i++) {
-                bool bool1 = (*input1++) > 0.0f;
-                bool bool2 = (*input2++) > 0.0f;
+                bool bool1 = (*input1++) != 0.0f;
+                bool bool2 = (*input2++) != 0.0f;
                 *output++ = (bool1 && bool2) ? 1.0f : 0.0f;
             }
         }
@@ -65,8 +65,8 @@ namespace l::nodegraph {
             auto input2 = inputs.at(1).GetIterator(numSamples);
             auto output = outputs.at(0).GetIterator(numSamples);
             for (int32_t i = 0; i < numSamples; i++) {
-                bool bool1 = (*input1++) > 0.0f;
-                bool bool2 = (*input2++) > 0.0f;
+                bool bool1 = (*input1++) != 0.0f;
+                bool bool2 = (*input2++) != 0.0f;
                 *output++ = (bool1 || bool2) ? 1.0f : 0.0f;
             }
         }
@@ -89,8 +89,8 @@ namespace l::nodegraph {
             auto input2 = inputs.at(1).GetIterator(numSamples);
             auto output = outputs.at(0).GetIterator(numSamples);
             for (int32_t i = 0; i < numSamples; i++) {
-                bool bool1 = (*input1++) > 0.0f;
-                bool bool2 = (*input2++) > 0.0f;
+                bool bool1 = (*input1++) != 0.0f;
+                bool bool2 = (*input2++) != 0.0f;
                 *output++ = (bool1 ^ bool2) ? 1.0f : 0.0f;
             }
         }
@@ -103,71 +103,101 @@ namespace l::nodegraph {
             NodeGraphOp(node, "Detector")
         {
             AddInput("In", 0.0f);
-            AddOutput("Bull", 0.0f);
-            AddOutput("Bear", 0.0f);
-            AddOutput("Peak", 0.0f);
-            AddOutput("Troph", 0.0f);
-            AddOutput("Lift", 0.0f);
-            AddOutput("Drop", 0.0f);
+            AddInput("Scale Trend", 0.5f, 1, 0.0f, 1.0f);
+            AddInput("Scale Reversal", 0.5f, 1, 0.0f, 1.0f);
+            AddInput("Scale Accel", 0.5f, 1, 0.0f, 1.0f);
+
+            AddOutput("Trend", 0.0f);
+            AddOutput("Reversal", 0.0f);
+            AddOutput("Accel", 0.0f);
+            AddOutput("Sum", 0.0f);
         }
 
         virtual ~GraphLogicalDetector() = default;
         virtual void Process(int32_t numSamples, std::vector<NodeGraphInput>& inputs, std::vector<NodeGraphOutput>& outputs) override {
-            auto input = inputs.at(0).GetIterator(numSamples);
-            auto outputBull = outputs.at(0).GetIterator(numSamples);
-            auto outputBear = outputs.at(1).GetIterator(numSamples);
-            auto outputPeak = outputs.at(2).GetIterator(numSamples);
-            auto outputTroph = outputs.at(3).GetIterator(numSamples);
-            auto outputBullish = outputs.at(4).GetIterator(numSamples);
-            auto outputBearish = outputs.at(5).GetIterator(numSamples);
+            auto input0 = inputs.at(0).GetIterator(numSamples);
+            auto input2 = inputs.at(1).GetIterator();
+            auto input3 = inputs.at(2).GetIterator();
+            auto input4 = inputs.at(3).GetIterator();
+
+            auto outputTrend = outputs.at(0).GetIterator(numSamples);
+            auto outputReversal = outputs.at(1).GetIterator(numSamples);
+            auto outputAccel = outputs.at(2).GetIterator(numSamples);
+            auto outputSum = outputs.at(3).GetIterator(numSamples);
+
+            float scaleTrend = (*input2++);
+            float scaleReversal = (*input3++);
+            float scaleAccel = (*input4++);
+            const float cSum = 1.0f / (scaleTrend + scaleReversal + scaleAccel); // scale to [-1,1]
+            scaleTrend *= cSum;
+            scaleReversal *= cSum;
+            scaleAccel *= cSum;
 
             for (int32_t i = 0; i < numSamples; i++) {
-                float in = (*input++);
-                float diff01 = in - mPrev1;
+                float in1 = (*input0++);
+
+                float diff01 = in1 - mPrev1;
                 float diff12 = mPrev1 - mPrev2;
-                float diff02 = in - mPrev2;
+                float diff23 = mPrev2 - mPrev3;
+                float diff34 = mPrev3 - mPrev4;
 
-                bool bull = false;
-                bool bear = false;
-                bool peak = false;
-                bool troph = false;
-                bool bullish = false;
-                bool bearish = false;
+                //float diff02 = in1 - mPrev2;
+                //float diff03 = in1 - mPrev3;
 
-                if (diff02 > 0.0f) {
-                    bull = true;
-                }
-                if (diff02 < 0.0f) {
-                    bear = true;
-                }
-                if (diff01 < 0.0f && diff12 > 0.0f) {
-                    peak = true;
-                }
-                if (diff01 > 0.0f && diff12 < 0.0f) {
-                    troph = true;
-                }
-                if (diff01 > 0.0f && diff12 > 0.0f && diff01 > diff12) {
-                    bullish = true;
-                }
-                if (diff01 < 0.0f && diff12 < 0.0f && diff01 < diff12) {
-                    bearish = true;
-                }
+                bool bull1 = diff01 > 0.0f;
+                bool bull2 = bull1 && diff12 > 0.0f;
+                bool bull3 = bull2 && diff23 > 0.0f;
+                bool bull4 = bull3 && diff34 > 0.0f;
 
-                *outputBull++ = bull ? 1.0f : 0.0f;
-                *outputBear++ = bear ? 1.0f : 0.0f;
-                *outputPeak++ = peak ? 1.0f : 0.0f;
-                *outputTroph++ = troph ? 1.0f : 0.0f;
-                *outputBullish++ = bullish ? 1.0f : 0.0f;
-                *outputBearish++ = bearish ? 1.0f : 0.0f;
+                bool bear1 = diff01 < 0.0f;
+                bool bear2 = bear1 && diff12 < 0.0f;
+                bool bear3 = bear2 && diff23 < 0.0f;
+                bool bear4 = bear3 && diff34 < 0.0f;
+                
+                bool troph1 = bull1 && diff12 < 0.0f;
+                bool troph2 = troph1 && diff23 < 0.0f;
+                bool troph3 = troph2 && diff34 < 0.0f;
 
+                bool peak1 = bear1 && diff12 > 0.0f;
+                bool peak2 = peak1 && diff23 > 0.0f;
+                bool peak3 = peak2 && diff34 > 0.0f;
+
+                bool bullish1 = bull1 && diff01 > diff12;
+                bool bullish2 = bullish1 && diff12 > diff23;
+                bool bullish3 = bullish2 && diff23 > diff34;
+
+                bool bearish1 = bear1 && diff01 < diff12;
+                bool bearish2 = bearish1 && diff12 < diff23;
+                bool bearish3 = bearish2 && diff23 < diff34;
+
+                float bull = (bull1 ? 1.0f : 0.0f) + (bull2 ? 1.0f : 0.0f) + (bull3 ? 1.0f : 0.0f) + (bull4 ? 1.0f : 0.0f);
+                float bear = (bear1 ? 1.0f : 0.0f) + (bear2 ? 1.0f : 0.0f) + (bear3 ? 1.0f : 0.0f) + (bear4 ? 1.0f : 0.0f);
+                float troph = (troph1 ? 1.0f : 0.0f) + (troph2 ? 1.0f : 0.0f) + (troph3 ? 1.0f : 0.0f);
+                float peak = (peak1 ? 1.0f : 0.0f) + (peak2 ? 1.0f : 0.0f) + (peak3 ? 1.0f : 0.0f);
+                float bullish = (bullish1 ? 1.0f : 0.0f) + (bullish2 ? 1.0f : 0.0f) + (bullish3 ? 1.0f : 0.0f);
+                float bearish = (bearish1 ? 1.0f : 0.0f) + (bearish2 ? 1.0f : 0.0f) + (bearish3 ? 1.0f : 0.0f);
+                
+                float trend = (bull - bear) * 0.25f;
+                float reversal = (troph - peak) * 0.33f;
+                float accel = (bullish - bearish) * 0.33f;
+
+                *outputTrend++ = trend;
+                *outputReversal++ = reversal;
+                *outputAccel++ = accel;
+                *outputSum++ = scaleTrend * trend + scaleReversal * reversal + scaleAccel * accel;
+
+                mPrev4 = mPrev3;
+                mPrev3 = mPrev2;
                 mPrev2 = mPrev1;
-                mPrev1 = in;
+                mPrev1 = in1;
             }
         }
 
     protected:
         float mPrev1 = 0.0f;
         float mPrev2 = 0.0f;
+        float mPrev3 = 0.0f;
+        float mPrev4 = 0.0f;
     };
 
 }
